b28e9d4ba07779a4e6816fc3409f1ca48e94ef89
Maggie Hillebrecht <hillebrecht@chapman.edu>
2026-03-17 16:44:13 -0700

    idk

--- Assets/Scripts/Core/PlayerController.cs
+++ Assets/Scripts/Core/PlayerController.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private PlayerInputReader input;
+
     private PlayerMovement playerMovement;
+    private PlayerSprint sprint; 
 
     void Awake()
     {
         if (input == null) input = GetComponent<PlayerInputReader>();
+
         playerMovement = GetComponent<PlayerMovement>();
+        sprint = GetComponent<PlayerSprint>(); 
     }
 
-    void Update()
+    void FixedUpdate()
     {
         Vector3 moveDir = new Vector3(input.Move.x, 0f, input.Move.y);
-        if (moveDir.magnitude > 1f) moveDir.Normalize();
-        playerMovement.SetMovementDirection(moveDir);
+
+        float sprintMultiplier = sprint != null ? sprint.GetCurrentSpeed() : 1f;
+
+        playerMovement.SetMovementDirection(moveDir, sprintMultiplier); 
     }
 }
